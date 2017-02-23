4c620aea2ca5ad983fea6bf2efec54d960ee0281
Dana Zukas <dana.zukas@gmail.com>
2017-02-22 20:44:00 -0500

    added jquery on seat hover

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 $ (document).ready(function(){
 
-$(".seat").css("<img id='theImg' src='https://www.google.com/imgres?imgurl=http%3A%2F%2Fimg.archiexpo.com%2Fimages_ae%2Fphoto-g%2F97760-3865537.jpg&imgrefurl=http%3A%2F%2Fwww.archiexpo.com%2Fprod%2Fforum-seating%2Fproduct-97760-951650.html&docid=GCDULNmXMigmsM&tbnid=37p_x-yKEf5cCM%3A&vet=1&w=300&h=300&bih=658&biw=1366&q=image%20of%20a%20%20stadium%20seat&ved=0ahUKEwiE2qrF96TSAhVl7IMKHVjMBYsQMwhrKCowKg&iact=mrc&uact=8'/>");
+  $('#rowOneSeatOne').hover(
+  function () {
+    originalText=$(this).text();
+    $(this).html('<p>Row: 1<br>Seat: 1</p>');
+  },function () {
+    $(this).text(originalText);
+  });
 
-
-
-
-
+  $('#rowOneSeatTwo').hover(
+  function () {
+    originalText=$(this).text();
+    $(this).html('<p>Row: 1<br>Seat: 2</p>');
+  },function () {
+    $(this).text(originalText);
+  });
 
 });

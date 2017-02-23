408dfd41e41df6ebc4d62b7977f068800a6e1ffb
John Stewart <stewartj232@detroitmi.gov>
2017-02-22 19:27:02 -0500

    added the form and paragraph to the html

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 $ (document).ready(function(){
 
-
-
-
-
+//$(".seat").append("<img id='theImg' src='https://www.google.com/imgres?imgurl=http%3A%2F%2Fimg.archiexpo.com%2Fimages_ae%2Fphoto-g%2F97760-3865537.jpg&imgrefurl=http%3A%2F%2Fwww.archiexpo.com%2Fprod%2Fforum-seating%2Fproduct-97760-951650.html&docid=GCDULNmXMigmsM&tbnid=37p_x-yKEf5cCM%3A&vet=1&w=300&h=300&bih=658&biw=1366&q=image%20of%20a%20%20stadium%20seat&ved=0ahUKEwiE2qrF96TSAhVl7IMKHVjMBYsQMwhrKCowKg&iact=mrc&uact=8'/>");
 
 
 
